b5f6d354beaa81d1bc0ccf31e5d9dffad76767df
Harditya Ramadhan <hardityarahmat@gmail.com>
2012-10-22 04:43:31 +0700

    Added cache system, new welcome page, and fix several bugs

---
+++ app/views/home/v_notfound.php
@@ -0,0 +1,45 @@
+<div class="container_12 gradient_black" style="width:100%;">
+    <div class="container_12">
+        <div class="grid_12" id="header">
+            <h3>Aries Micro Framework</h3>
+        </div>
+    </div>
+</div>
+<div class="container_12 gradient_orange" style="width:100%;">
+    <div class="container_12">
+        <div class="grid_12">
+            <br />
+        </div>
+    </div>
+</div>
+<div class="container_12">
+    <div class="grid_12" id="content">
+        <h3 id="horay">
+            ERROR 404. We couldn't find what page your looking for.
+        </h3>
+    </div>
+</div>
+<div class="container_12" id="footerContainer">
+    <div class="container_12 footer">
+        <div class="grid_4 footer_item">
+            <h3>Contribute</h3>
+            <a href="https://github.com/freeskys/aries-framework-php" target="_blank">Fork my Github</a>
+        </div>
+        <div class="grid_4 footer_item">
+            <h3>Developer</h3>
+            <a href="https://twitter.com/freeskys">@freeskys</a>
+        </div>
+        <div class="grid_4 footer_item">
+            <h3>Framework included in Aries</h3>
+            <a href="http://960.gs/">960 Grid System</a> by Nathan Smith<br />
+        </div>
+    </div>
+</div>
+<div class="container_12 gradient_orange" id="footerContainer2">
+    <div class="container_12">
+        <div class="grid_12 copyright">
+            <br />
+            Copyright &copy; 2012 Harditya Rahmat Ramadhan | Aries Micro Framework is released under MIT License.
+        </div>
+    </div>
+</div>
